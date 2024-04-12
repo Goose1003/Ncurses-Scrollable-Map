8d9e0297fa2d88c5764a2eed1ee73e5952a619ca
Goose1003 <stgoosey@gmail.com>
2024-04-11 22:37:03 -0700

    WORKING SCROLLABLE MAP WOOOOOOOOOOO

--- main.cc
+++ main.cc
@@ -1,23 +1,39 @@
 #include <ncurses.h>
-
-char tiles[100][100];
+#include "map.h"
 
 struct Player {
     int y;
     int x;
     WINDOW* win;
-    Player(WINDOW* w) : win(w), x(50), y(50) {}
-    void display() {
+    Player(WINDOW* w, int newY, int newX) : win(w), y(newY), x(newX) {};
+    void mvup() { if (y < 99) y++; }
+    void mvdown() { if (y > 0) y--; }
+    void mvright() { if (x > 0) x--; }
+    void mvleft() { if (x < 99) x++; }
+    void scrollMap() {
+        int xOffSet = 42, yOffSet = 11;
         wclear(win);
+        for (int i = 0; i < 32; i++) {
+            for (int j = 0; j < 98; j++) {
+                mvwaddch(win, (y - yOffSet) + i, (x - xOffSet) + j, s[i].at(j));
+            }
+        }
+    }
+    void display() {
+        displaypos();
+        scrollMap();
         box(win, (int)'|', (int)'-');
-        mvwprintw(win, 1, 1, "y: %d", y);
-        mvwprintw(win, 2, 1, "x: %d", x);
+        mvwaddch(win, 12, 43, '@');
+        wmove(win, 12, 43);
         wrefresh(win);
     }
-    void mvup() { if (y > 0) y--; }
-    void mvdown() { if (y < 100) y++; }
-    void mvright() { if (x < 100) x++; }
-    void mvleft() { if (x > 0) x--; }
+    void displaypos() {
+        WINDOW* debug = newwin(4, 12, 0, 88);
+        box(debug, (int)'|', (int)'-');
+        mvwprintw(debug, 1, 1, "y: %d", y);
+        mvwprintw(debug, 2, 1, "x: %d", x);
+        wrefresh(debug);
+    }
 };
 
 int getmv(Player* p){
@@ -45,9 +61,8 @@ int getmv(Player* p){
 int main (){
     initscr();
     noecho();
-    tiles[50][50] = '0';
-    WINDOW* mainwin = newwin(25, 80, 0, 0);
-    Player* p = new Player(mainwin);
+    WINDOW* mainwin = newwin(25, 87, 0, 0);
+    Player* p = new Player(mainwin, 0, 0);
     refresh();
     do {
         p->display();
