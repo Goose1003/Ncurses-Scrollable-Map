ffeb819cd7a725e4e3dabf2d809f812e4ba5601b
Goose1003 <stgoosey@gmail.com>
2024-04-14 19:09:30 -0700

    changed win to stdscr

--- main.cc
+++ main.cc
@@ -4,28 +4,27 @@
 struct Player {
     int y;
     int x;
-    WINDOW* win;
-    Player(WINDOW* w, int newY, int newX) : win(w), y(newY), x(newX) {};
+    Player(int newY, int newX) : y(newY), x(newX) {};
     void mvup() { y--; }
     void mvdown() { y++; }
     void mvright() { x++; }
     void mvleft() { x--; }
     void scrollMap() {
         int xOffSet = 43, yOffSet = 12;
-        wclear(win);
+        clear();
         for (int i = 0; i < map1.size(); i++) {
             for (int j = 0; j < map1[i].size(); j++) {
-                mvwaddch(win, (-1 * y + yOffSet) + i, (-1 * x + xOffSet) + j, map1[i][j]);
+                mvaddch((-1 * y + yOffSet) + i, (-1 * x + xOffSet) + j, map1[i][j]);
             }
         }
     }
     void display() {
         displaypos();
         scrollMap();
-        box(win, (int)'|', (int)'-');
-        mvwaddch(win, 12, 43, '@');
-        wmove(win, 12, 43);
-        wrefresh(win);
+        //box(win, (int)'|', (int)'-');
+        mvaddch(12, 43, '@');
+        move(12, 43);
+        refresh();
     }
     void displaypos() {
         WINDOW* debug = newwin(4, 12, 0, 88);
@@ -62,7 +61,7 @@ int main (){
     initscr();
     noecho();
     WINDOW* mainwin = newwin(25, 87, 0, 0);
-    Player* p = new Player(mainwin, 0, 0);
+    Player* p = new Player(0, 0);
     refresh();
     do {
         p->display();
